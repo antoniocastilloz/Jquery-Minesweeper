3ffd32c52811563ccefc722764b14bfba705f06a
antoniocastilloz <47866299+antoniocastilloz@users.noreply.github.com>
2019-12-20 09:41:17 -0300

    Criação de classe css e funções para limpar e preencher o tabuleiro
    -Criação de classes para setting do tamanho do tabuleiro.
    - Criação de uma função para retirada de classes adicionais no tabuleiro.
    - Adição de um for para preencher o tabuleiro.

--- calc/script.js
+++ calc/script.js
@@ -1,13 +1,47 @@
 let assets = {Numbers: [1,2,3,4], Bomb: "&#x1F4A3;", Flag:"&#x1F6A9;", Explosion:"&#x1F4A5;"}
 
-function startGame() {
-    let boardSize = $('input[name="radio-box"]:checked').next().text();
-    let numMines = $('#numMines').val();
-    let size = boardSize.split(' x ')[0];
 
-    if ((Math.pow(Number(size), 2) / 2) >= Number(numMines)) {
-        // TODO: ações do game
+
+function startGame() {
+    let boardSizeRadio = $('input[name="radio-box"]:checked').next().text();
+    let numMinesInput = $('#numMines').val();
+    let size = boardSizeRadio.split(' x ')[0];
+    console.log(size)
+    if ((Math.pow(Number(size), 2) / 2) >= Number(numMinesInput)) {
+        switch(size){
+            case "5":
+                $("#gameBoard").empty();
+                removeAdditionalWidthClassesInGameBoard("gameBoardWidth10x10","gameBoardWidth15x15")
+                for(var i = 0; i < 24; i++){
+                    $("#gameBoard").addClass("gameBoardWidth5x5")
+                    $("#gameBoard").append('<div class="defaultSquareBoard">&#x1F4A3;</div>')
+                }
+            break;
+            case "10":
+                $("#gameBoard").empty();
+                removeAdditionalWidthClassesInGameBoard("gameBoardWidth5x5","gameBoardWidth15x15")
+                for(var i = 0; i < 99; i++){ 
+                    $("#gameBoard").addClass("gameBoardWidth10x10")
+                    $("#gameBoard").append('<div class="defaultSquareBoard">&#x1F4A3;</div>')
+                }
+            break;
+            case "15":
+                $("#gameBoard").empty();
+                removeAdditionalWidthClassesInGameBoard("gameBoardWidth10x10","gameBoardWidth5x5")
+                for(var i = 0; i < 224; i++){
+                    $("#gameBoard").addClass("gameBoardWidth15x15")
+                    $("#gameBoard").append('<div class="defaultSquareBoard">&#x1F4A3;</div>')
+                }
+            break;
+        }
     } else {
         M.toast({ html: 'Insira um número de minas menor ou igual a metade do tamnho do tabuleiro escolhido!', classes: 'rounded teal lighten-1' })
+    }
+}
+
+function removeAdditionalWidthClassesInGameBoard(additionalClass1,additionalClass2){
+    if($("#gameBoard").hasClass(additionalClass1) || $("#gameBoard").hasClass(additionalClass2) == true){
+        $("#gameBoard").removeClass(additionalClass1)
+        $("#gameBoard").removeClass(additionalClass2)
     }
 }
