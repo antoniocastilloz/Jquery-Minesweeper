ce2c489478df47481391e39dadd4fcd7441e0e7a
antoniocastilloz <47866299+antoniocastilloz@users.noreply.github.com>
2019-12-19 22:37:11 -0300

    Refatoração de código e definição dos assests
    - Alteração da cor do texto do input para seleção do número de minas.
    - Alteração de ids para padrão camelCase e Inglês
    - Retirada do margin-bottom da row que está dentro do container.
    - Definição de alguns assests

--- calc/script.js
+++ calc/script.js
@@ -1,6 +1,8 @@
+let assets = {Numbers: [1,2,3,4], Bomb: "&#x1F4A3;", Flag:"&#x1F6A9;", Explosion:"&#x1F4A5;"}
+
 function startGame() {
     let boardSize = $('input[name="radio-box"]:checked').next().text();
-    let numMines = $('#num-minas').val();
+    let numMines = $('#numMines').val();
     let size = boardSize.split(' x ')[0];
 
     if ((Math.pow(Number(size), 2) / 2) >= Number(numMines)) {
