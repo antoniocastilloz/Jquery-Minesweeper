91b998d5112937880a27a984b30e9cffb86bdb8f
antoniocastilloz <47866299+antoniocastilloz@users.noreply.github.com>
2019-12-21 12:44:33 -0300

    Improvements no tabuleiro e quadrados do mesmo.
    - Alteração e criação de classes css para os quadrados do tabuleiro e tabuleiro.
    - Criação de função para click dos quadrados.
    - Inserção dinâmica do tabuleiro no html.
    - Criação de função para o render do tabuleiro.

--- game/script.js
+++ game/script.js
@@ -6,46 +6,29 @@ function startGame() {
     let boardSizeRadio = $('input[name="radio-box"]:checked').next().text();
     let numMinesInput = $('#numMines').val();
     let size = boardSizeRadio.split(' x ')[0];
-    
+    cleanGameBoard()
+    configGame(numMinesInput, size)
+    squareClick()
+}
+
+function configGame(numMinesInput, size) {
     if (numMinesInput == "") {
         M.toast({ html: 'O número de minas deve ser preenchido !', classes: 'rounded teal lighten-1' })
-    } else if(Number(numMinesInput) == 0){
+    } else if (Number(numMinesInput) == 0) {
         M.toast({ html: 'O número de minas deve maior que 0 !', classes: 'rounded teal lighten-1' })
-    }else if ((Math.pow(Number(size), 2) / 2) >= Number(numMinesInput)) {
-        switch (size) {
-            case "5":
-                cleanGameBoard()
-                for (var i = 0; i < 24; i++) {
-                    $("#gameBoard").addClass("gameBoardWidth5x5")
-                    $("#gameBoard").append('<div class="defaultSquareBoard">&#x1F4A3;</div>')
-                }
-                break;
-            case "10":
-                cleanGameBoard("gameBoardWidth5x5", "gameBoardWidth15x15")
-                for (var i = 0; i < 99; i++) {
-                    $("#gameBoard").addClass("gameBoardWidth10x10")
-                    $("#gameBoard").append('<div class="defaultSquareBoard">&#x1F4A3;</div>')
-                }
-                break;
-            case "15":
-                cleanGameBoard("gameBoardWidth10x10", "gameBoardWidth5x5")
-                for (var i = 0; i < 224; i++) {
-                    $("#gameBoard").addClass("gameBoardWidth15x15")
-                    $("#gameBoard").append('<div class="defaultSquareBoard">&#x1F4A3;</div>')
-                }
-                break;
-        }
+    } else if ((Math.pow(Number(size), 2) / 2) >= Number(numMinesInput)) {
+        verifyIfGameBoardExists()
+        renderGameBoardAccordingSize(size)
     } else {
         M.toast({ html: 'Insira um número de minas menor ou igual a metade do tamanho do tabuleiro escolhido !', classes: 'rounded teal lighten-1' })
     }
 }
 
-function onlyNumbersInInput(input){
-    $(input).keypress(function(key) {
-        if(key.charCode < 48 || key.charCode > 57) return false;
+function onlyNumbersInInput(input) {
+    $(input).keypress(function (key) {
+        if (key.charCode < 48 || key.charCode > 57) return false;
     });
 }
-
 
 function removeAdditionalWidthClassesInGameBoard() {
     if ($("#gameBoard").hasClass("gameBoardWidth5x5") == true) {
@@ -60,4 +43,39 @@ function removeAdditionalWidthClassesInGameBoard() {
 function cleanGameBoard() {
     $("#gameBoard").empty();
     removeAdditionalWidthClassesInGameBoard()
+}
+
+function squareClick() {
+    $("div[name='square']").click(function () {
+        $(this).removeClass("squareUnpressed").addClass("squarePressed")
+    })
+}
+
+function verifyIfGameBoardExists(){
+    if ($("#gameBoard").length == 0) {
+        $('#game').append('<div id="gameBoard" class="col s1"></div>')
+    }
+}
+
+function renderGameBoardAccordingSize(size){
+    switch (size) {
+        case "5":
+            for (var i = 0; i < 25; i++) {
+                $("#gameBoard").addClass("gameBoardWidth5x5")
+                $("#gameBoard").append('<div id="square' + i + '" name="square" class="squareUnpressed"></div>')
+            }
+            break;
+        case "10":
+            for (var i = 0; i < 100; i++) {
+                $("#gameBoard").addClass("gameBoardWidth10x10")
+                $("#gameBoard").append('<div id="square' + i + '" name="square" class="squareUnpressed"></div>')
+            }
+            break;
+        case "15":
+            for (var i = 0; i < 225; i++) {
+                $("#gameBoard").addClass("gameBoardWidth15x15")
+                $("#gameBoard").append('<div id="square' + i + '" name="square" class="squareUnpressed"></div>')
+            }
+            break;
+    }
 }
