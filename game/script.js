2465c6cd3a9c016043d0cac655bc13b2298d737f
antoniocastilloz <47866299+antoniocastilloz@users.noreply.github.com>
2020-01-05 19:49:55 -0300

    Criaçãp de função para exibir quadrados de valor 0 ao redor de um quadrado clickado.

--- game/script.js
+++ game/script.js
@@ -3,10 +3,10 @@ let boardGame;
 let hits;
 let expectedHits;
 
-var modalLoseElement = document.querySelector('#modalLose');
-var modalWinElement = document.querySelector('#modalWin');
-var modalLose = M.Modal.init(modalLoseElement);
-var modalWin = M.Modal.init(modalWinElement);
+let modalLoseElement = document.querySelector('#modalLose');
+let modalWinElement = document.querySelector('#modalWin');
+let modalLose = M.Modal.init(modalLoseElement);
+let modalWin = M.Modal.init(modalWinElement);
 
 onlyNumbersInInput("#numMines");
 
@@ -29,6 +29,7 @@ function configGame(numMinesInput, size) {
     } else if (Number(numMinesInput) == 0) {
         M.toast({ html: 'O número de minas deve maior que 0 !', classes: 'rounded teal lighten-1' });
     } else if ((Math.pow(Number(size), 2) / 2) >= Number(numMinesInput)) {
+        showGameBoardHideGameFilter()
         verifyIfGameBoardExists();
         createBoardGame(size, numMinesInput);
         renderGameBoardAccordingSize(size);
@@ -80,9 +81,9 @@ function createBoardGame(size, numMinesInput) {
 //Todo: apagar após os testes
 function tempPrint() {
     console.log(boardGame)
-    for (let j = 0; j < 5; j++) {
-        console.log('\'' + boardGame[j][0] + '\' - \'' + boardGame[j][1] + '\' - \'' + boardGame[j][2] + '\' - \'' + boardGame[j][3] + '\' - \'' + boardGame[j][4] + '\'');
-    }
+    // for (let j = 0; j < 5; j++) {
+    //     console.log('\'' + boardGame[j][0] + '\' - \'' + boardGame[j][1] + '\' - \'' + boardGame[j][2] + '\' - \'' + boardGame[j][3] + '\' - \'' + boardGame[j][4] + '\'');
+    // }
 }
 
 function onlyNumbersInInput(input) {
@@ -119,24 +120,32 @@ function leftClick(size) {
                 $(this).append(boardGame[positionX][positionY]);
                 $(this).removeClass("squareUnpressed").addClass("squarePressed");
                 hits++;
+                showAllAroundSquaresAssets(boardGame, positionX, positionY)
             } else {
                 //Todo: melhorar animação das minas e declarar fim de jogo
 
-                $("div[name='square']").each(function () {
-                    if (boardGame[$(this).attr('value').slice(0, 1)][$(this).attr('value').slice(2)] == assets.Bomb) {
-                        $("div[name='square']").removeClass("squareUnpressed").addClass("squarePressed");
-                        $(this).append(assets.Bomb);
-                        setTimeout(function () { $(this).text(''); $(this).append(assets.Explosion); }.bind($(this)), 1000);
-                    }
-                })
-                setTimeout(function () { modalLose.open(); }, 1000);
+                showAllBombs()
+                explodeBombs()
+                setTimeout(function () {
+                    modalLose.open();
+                    $(".modal-close,.modal-overlay").click(function () {
+                        hideGameBoardShowGameFilter()
+                    })
+                }, 1500);
+
             }
 
-            //Todo: declarar vitória
             if (hits == expectedHits) {
-                setTimeout(function () { modalWin.open(); }, 1000);
+                setTimeout(function () {
+                    modalWin.open();
+                    showAllBombs()
+                    $(".modal-close,.modal-overlay").click(function () {
+                        hideGameBoardShowGameFilter()
+                    })
+                }, 1000);
             }
         }
+
     })
 }
 
@@ -172,6 +181,134 @@ function renderGameBoardAccordingSize(size) {
     }
 }
 
+function hideGameBoardShowGameFilter() {
+    $('#gameBoard').addClass('visibilityHidden')
+    $('#gameFilter').removeClass('visibilityHidden')
+}
 
+function showGameBoardHideGameFilter() {
+    $('#gameBoard').removeClass('visibilityHidden')
+    $('#gameFilter').addClass('visibilityHidden')
+}
 
+function showAllBombs() {
+    $("div[name='square']").each(function () {
+        if (boardGame[$(this).attr('value').slice(0, 1)][$(this).attr('value').slice(2)] == assets.Bomb) {
+            $("div[name='square']").removeClass("squareUnpressed").addClass("squarePressed");
+            $(this).append(assets.Bomb);
+        }
+    })
+}
+
+function explodeBombs() {
+    $("div[name='square']").each(function () {
+        if (boardGame[$(this).attr('value').slice(0, 1)][$(this).attr('value').slice(2)] == assets.Bomb) {
+            setTimeout(function () { $(this).text(''); $(this).append(assets.Explosion); }.bind($(this)), 1500);
+        }
+    })
+}
+
+function showAllAroundSquaresAssets(boardGame, positionX, positionY) {
+    if (boardGame[positionX][positionY] == 0) {
+        let positionXLeft = positionX - 1
+        let positionXRight = positionX + 1
+        let positionYUp = positionY - 1
+        let positionYDown = positionY + 1
+
+        let northWestSquare = $("div[value='" + positionXLeft + "." + positionYUp + "']")
+        let northSquare = $("div[value='" + positionXLeft + "." + positionY + "']")
+        let northEastSquare = $("div[value='" + positionXLeft + "." + positionYDown + "']")
+        let westSquare = $("div[value='" + positionX + "." + positionYUp + "']")
+        let eastSquare = $("div[value='" + positionX + "." + positionYDown + "']")
+        let southWestSquare = $("div[value='" + positionXRight + "." + positionYUp + "']")
+        let southSquare = $("div[value='" + positionXRight + "." + positionY + "']")
+        let southEastSquare = $("div[value='" + positionXRight + "." + positionYDown + "']")
+
+        try {
+            if (boardGame[positionXLeft][positionYUp] != assets.Bomb && northWestSquare.length == 1 && northWestSquare.text() == "") {
+                northWestSquare.removeClass("squareUnpressed").addClass("squarePressed");
+                northWestSquare.append(boardGame[positionXLeft][positionYUp]);
+                hits++;
+            }
+        } catch (e) {
+            console.log(e)
+        }
+        try {
+            if (boardGame[positionXLeft][positionY] != assets.Bomb && northSquare.length == 1 && northSquare.text() == "") {
+                northSquare.removeClass("squareUnpressed").addClass("squarePressed");
+                northSquare.append(boardGame[positionXLeft][positionY]);
+                hits++;
+            }
+        } catch (e) {
+            console.log(e)
+        }
+        try {
+            if (boardGame[positionXLeft][positionYDown] != assets.Bomb && northEastSquare.length == 1 && northEastSquare.text() == "") {
+                northEastSquare.removeClass("squareUnpressed").addClass("squarePressed");
+                northEastSquare.append(boardGame[positionXLeft][positionYDown]);
+                hits++;
+            }
+        } catch (e) {
+            console.log(e)
+        }
+        try {
+            if (boardGame[positionX][positionYUp] != assets.Bomb && westSquare.length == 1 && westSquare.text() == "") {
+                westSquare.removeClass("squareUnpressed").addClass("squarePressed");
+                westSquare.append(boardGame[positionX][positionYUp]);
+                hits++;
+            }
+        } catch (e) {
+            console.log(e)
+        }
+        try {
+            if (boardGame[positionX][positionYDown] != assets.Bomb && eastSquare.length == 1 && eastSquare.text() == "") {
+                eastSquare.removeClass("squareUnpressed").addClass("squarePressed");
+                eastSquare.append(boardGame[positionX][positionYDown]);
+                hits++;
+            }
+        } catch (e) {
+            console.log(e)
+        }
+        try {
+            if (boardGame[positionXRight][positionYUp] != assets.Bomb && southWestSquare.length == 1 && southWestSquare.text() == "") {
+                southWestSquare.removeClass("squareUnpressed").addClass("squarePressed");
+                southWestSquare.append(boardGame[positionXRight][positionYUp]);
+                hits++;
+            }
+        } catch (e) {
+            console.log(e)
+        }
+        try {
+            if (boardGame[positionXRight][positionY] != assets.Bomb && southSquare.length == 1 && southSquare.text() == "") {
+                southSquare.removeClass("squareUnpressed").addClass("squarePressed");
+                southSquare.append(boardGame[positionXRight][positionY]);
+                hits++;
+            }
+        } catch (e) {
+            console.log(e)
+        }
+        try {
+            if (boardGame[positionXRight][positionYDown] != assets.Bomb && southEastSquare.length == 1 && southEastSquare.text() == "") {
+                southEastSquare.removeClass("squareUnpressed").addClass("squarePressed");
+                southEastSquare.append(boardGame[positionXRight][positionYDown]);
+                hits++;
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    }
+    console.log(hits)
+}
+
+function showOneSquareAround(boardGame,positionX,positionY){
+    try {
+        if (boardGame[positionXRight][positionYDown] != assets.Bomb && southEastSquare.length == 1 && southEastSquare.text() == "") {
+            southEastSquare.removeClass("squareUnpressed").addClass("squarePressed");
+            southEastSquare.append(boardGame[positionXRight][positionYDown]);
+            hits++;
+        }
+    } catch (e) {
+        console.log(e)
+    }
+}
 
